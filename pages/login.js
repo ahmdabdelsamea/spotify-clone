8b37863ce7e38f1fb65fe82d5ae2eb3466038985
Ahmed Abdelsamea <94923716+ahmdabdelsamea@users.noreply.github.com>
2021-12-12 19:13:30 +0200

    middleware done

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,19 @@
 import { getProviders, signIn } from 'next-auth/react';
+import Image from 'next/image';
 
-// import SpotifyLogo from '../assets/Spotify-Logo.wine.svg';
+import SpotifyLogo from '../assets/spotify_logo.svg';
 
 function Login({ providers }) {
 	return (
 		<div className='flex flex-col items-center justify-center bg-black min-h-screen'>
-			<img className='w-80 mb-8' src='/spotify_logo.svg' />
+			<Image
+				src={SpotifyLogo}
+				alt='Spotify'
+				width={400}
+				// height={100}
+			/>
 			{Object.values(providers).map((provider) => (
-				<div key={provider.name}>
+				<div key={provider.name} className='mt-5'>
 					<button
 						className='bg-[#18D860] text-lg font-bold text-white w-80 p-3 rounded-full cursor-pointer hover:bg-[#00a13e]'
 						onClick={() => signIn(provider.id, { callbackUrl: '/' })}
