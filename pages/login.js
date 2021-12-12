eb34deea0709b000fb1e49ac0c14aa34c7253057
Ahmed Abdelsamea <94923716+ahmdabdelsamea@users.noreply.github.com>
2021-12-12 16:29:01 +0200

    signin pages done

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,21 @@
-import { getProviders, singIn } from 'next-auth/react';
+import { getProviders, signIn } from 'next-auth/react';
+
+// import SpotifyLogo from '../assets/Spotify-Logo.wine.svg';
 
 function Login({ providers }) {
 	return (
-		<div>
-			<h1>Login</h1>
+		<div className='flex flex-col items-center justify-center bg-black min-h-screen'>
+			<img className='w-80 mb-8' src='/spotify_logo.svg' />
+			{Object.values(providers).map((provider) => (
+				<div key={provider.name}>
+					<button
+						className='bg-[#18D860] text-lg font-bold text-white w-80 p-3 rounded-full cursor-pointer hover:bg-[#00a13e]'
+						onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+					>
+						Login with {provider.name}
+					</button>
+				</div>
+			))}
 		</div>
 	);
 }
