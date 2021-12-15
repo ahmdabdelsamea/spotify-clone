efe21468a577f81b3259ad4d66bd293ecf94bbee
Ahmed Abdelsamea <94923716+ahmdabdelsamea@users.noreply.github.com>
2021-12-15 15:58:11 +0200

    final commit

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,54 @@
 import Head from 'next/head';
-import { getSession } from 'next-auth/react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { getSession, useSession } from 'next-auth/react';
 
 import Sidebar from '../components/Sidebar';
 import Spotify from '../components/Spotify';
 import Player from '../components/Player';
 
 export default function Home() {
+	const router = useRouter();
+	const { data: session, status } = useSession();
+
+	useEffect(() => {
+		if (status === 'unauthenticated') {
+			router.push('/login');
+		}
+	}, [status]);
+
 	return (
 		<div className='bg-black h-screen overflow-hidden'>
-			<Head>
-				<title>Spotify Clone</title>
-				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
-				<meta
-					name='description'
-					content='Spotify Clone with Next.js and TailwindCSS'
-				/>
-				<link
-					rel='shortcut icon'
-					href='/images/spotify-logo.png'
-					type='image/png'
-				/>
-			</Head>
-			<main className='flex'>
-				<Sidebar />
-				<Spotify />
-			</main>
+			{status === 'loading' || session === undefined || session === null ? (
+				<h1 className='text-white text-2xl font-semibold p-10'>Loading...</h1>
+			) : (
+				<>
+					<Head>
+						<title>Spotify Clone</title>
+						<meta
+							name='viewport'
+							content='initial-scale=1.0, width=device-width'
+						/>
+						<meta
+							name='description'
+							content='Spotify Clone with Next.js and TailwindCSS'
+						/>
+						<link
+							rel='shortcut icon'
+							href='/images/spotify-logo.png'
+							type='image/png'
+						/>
+					</Head>
+					<main className='flex'>
+						<Sidebar />
+						<Spotify />
+					</main>
 
-			<div className='sticky bottom-0'>
-				<Player />
-			</div>
+					<div className='sticky bottom-0'>
+						<Player />
+					</div>
+				</>
+			)}
 		</div>
 	);
 }
--- pages/login.js
+++ pages/login.js
@@ -17,22 +17,28 @@ function Login({ providers }) {
 
 	return (
 		<div className='flex flex-col items-center justify-center bg-black min-h-screen'>
-			<Image
-				src={SpotifyLogo}
-				alt='Spotify'
-				width={400}
-				// height={100}
-			/>
-			{Object.values(providers).map((provider) => (
-				<div key={provider.name} className='mt-5'>
-					<button
-						className='bg-[#18D860] text-lg font-bold text-white w-80 p-3 rounded-full cursor-pointer hover:bg-[#00a13e]'
-						onClick={() => signIn(provider.id, { callbackUrl: '/' })}
-					>
-						Login with {provider.name}
-					</button>
-				</div>
-			))}
+			{status === 'loading' || session === undefined ? (
+				<h1 className='text-white text-2xl font-semibold p-10'>Loading...</h1>
+			) : (
+				<>
+					<Image
+						src={SpotifyLogo}
+						alt='Spotify'
+						width={400}
+						// height={100}
+					/>
+					{Object.values(providers).map((provider) => (
+						<div key={provider.name} className='mt-5'>
+							<button
+								className='bg-[#18D860] text-lg font-bold text-white w-80 p-3 rounded-full cursor-pointer hover:bg-[#00a13e]'
+								onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+							>
+								Login with {provider.name}
+							</button>
+						</div>
+					))}
+				</>
+			)}
 		</div>
 	);
 }
